4ad2ee39445e5cc6a7d18c76252a03093ea8de92
yasu.miyazaki <kizayami.yasu@gmail.com>
2018-05-17 12:41:49 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,10 +5,10 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] ='メッセージを投稿しました。'
+      flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
@@ -20,16 +20,17 @@ class MicropostsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+
   private
-  
+
   def micropost_params
     params.require(:micropost).permit(:content)
   end
-  
+
   def correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
-    redirect_to root_url
+      redirect_to root_url
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create] do
     member do
@@ -15,5 +15,4 @@ Rails.application.routes.draw do
 
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-  
 end
